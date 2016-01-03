556fa38b012a9536c0d850a16381dbb999f3d186
fr <francois.reymond10@gmail.com>
2016-01-03 20:54:31 +0100

    VueIA qui s'affiche!!

--- menhir/src/vue/VuePaquetDeRessourcesIA.java
+++ menhir/src/vue/VuePaquetDeRessourcesIA.java
@@ -4,6 +4,8 @@ import java.awt.Dimension;
 import java.util.Iterator;
 import java.util.Observable;
 
+import javax.swing.SwingUtilities;
+
 import modele.PaquetDeRessourcesDeJoueur;
 import Ressources.Ressources;
 
@@ -15,26 +17,39 @@ public class VuePaquetDeRessourcesIA extends VuePaquetDeRessourcesDeJoueur {
 			boolean avancee) {
 		super(paquetDeRessourcesDeJoueur, r, avancee);
 		this.setPreferredSize(new Dimension(250, 640));
+		tempTexte2 += referencePaquetDeRessourcesDeJoueur.getJoueur().getNom();
+		this.nomDuJoueur.setText(tempTexte2);
+		this.ajoutPanneau(nomDuJoueur, 10, 0);
+		
+		
 		
 	}
 
 	public void update(Observable arg0, Object arg1) {
+		Runnable myRunnable = new Runnable() {
+			public void run() {
 		if (!referencePaquetDeRessourcesDeJoueur.getPaquetsDeCartes().get("Cartes Ingredients").isEmpty()) {
+			
+
 			if (!tempVueCartes1.isEmpty()){
 				for (Iterator <VueCarte>it=tempVueCartes1.iterator();it.hasNext();){
 					VuePaquetDeRessourcesIA.this.remove(it.next());
 				}
 				tempVueCartes1.clear();
+			}
 			for (int i = 0; i < referencePaquetDeRessourcesDeJoueur.getPaquetsDeCartes().get("Cartes Ingredients").size(); i++) {
+				System.out.println("update2");
+
 				if (!referencePaquetDeRessourcesDeJoueur.getPaquetsDeCartes().get("Cartes Ingredients").get(i).isEstUtilise()) {
-					tempVueCartes1.add(new VueCarteIngredient(
+						tempVueCartes1.add(new VueCarteIngredient(
 							referencePaquetDeRessourcesDeJoueur.getPaquetsDeCartes().get("Cartes Ingredients").get(i), referenceRessources, TAILLE_CARTE,
 							TAILLE_CARTE, false));
-					VuePaquetDeRessourcesIA.this.ajoutPanneau(tempVueCartes1.get(i), (i%2)*TAILLE_CARTE, (i/2)*TAILLE_CARTE);
+					VuePaquetDeRessourcesIA.this.ajoutPanneau(tempVueCartes1.get(i), (i%2)*TAILLE_CARTE, 20+(i/2)*TAILLE_CARTE);
 					System.out.println("trol");
 				}
 			}
 		}
+		
 //		 carte champ
 		if (!referencePaquetDeRessourcesDeJoueur.getPaquetsDeCartes().get("Cartes Champs").isEmpty() && tempVueCartes2.isEmpty()) {
 			tempVueCartes2.add(new VueCarteChamp(
@@ -78,13 +93,20 @@ public class VuePaquetDeRessourcesIA extends VuePaquetDeRessourcesDeJoueur {
 		tempTexte1="";
 		tempTexte1+=referencePaquetDeRessourcesDeJoueur.getGrainesDeMenhir();
 		VuePaquetDeRessourcesIA.this.nombreDeGraines.setText(tempTexte1);
-		VuePaquetDeRessourcesIA.this.ajoutPanneau(nombreDeGraines, 355, 40);
+		VuePaquetDeRessourcesIA.this.ajoutPanneau(nombreDeGraines, 0, 125);
 		graines.clear();
 		for (int i = 0; i < VuePaquetDeRessourcesIA.this.referencePaquetDeRessourcesDeJoueur.getGrainesDeMenhir(); i++) {
 			graines.add(new VueImage(referenceRessources.getImageGraine(), 24, 10));
-			VuePaquetDeRessourcesIA.this.ajoutPanneau(graines.get(i), 5 + (i % 3) * 26, 10 + (i / 3) * 11);
+			VuePaquetDeRessourcesIA.this.ajoutPanneau(graines.get(i), 2*TAILLE_CARTE+5 + (i % 3) * 26, TAILLE_CARTE+10 + (i / 3) * 11);
 		}
 
-		}
+		
+		VuePaquetDeRessourcesIA.this.repaint();
+		VuePaquetDeRessourcesIA.this.revalidate();
+			}
+		};
+		SwingUtilities.invokeLater(myRunnable);
 	}
+			
+	
 }
